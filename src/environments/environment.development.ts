9f371379784ec067baee32966f5f697102b2d1fb
jon.apiscope.empresa <jon.apiscope.empresa@gmail.com>
2024-11-01 20:17:50 +0000

    Proyecyo funcional

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,10 @@
 export const environment = {
   firebase: {
-    projectId: 'contacts-55e11',
-    appId: '1:161751204246:web:2633c178e847015166ddb7',
-    storageBucket: 'contacts-55e11.appspot.com',
-    apiKey: 'AIzaSyBViAlIFdAXXdljC0vQDl9WRravzN5Jz8Q',
-    authDomain: 'contacts-55e11.firebaseapp.com',
-    messagingSenderId: '161751204246',
+    projectId: 'ccr-status-comercio',
+    appId: '1:429347182679:web:af87b1a3a276e4001cab12',
+    storageBucket: 'ccr-status-comercio.firebasestorage.app',
+    apiKey: 'AIzaSyCD7iqzIqi1gI76-g7w4wG48ZSOU8J8q4Y',
+    authDomain: 'ccr-status-comercio.firebaseapp.com',
+    messagingSenderId: '429347182679',
   },
 };
